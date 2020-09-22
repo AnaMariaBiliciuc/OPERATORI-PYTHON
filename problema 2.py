80328a70af63bad5c926215b57d9797e6f2a55ca
AnaMariaBiliciuc <70842937+AnaMariaBiliciuc@users.noreply.github.com>
2020-09-22 17:51:50 +0300

    Add files via upload

---
+++ problema 2.py
@@ -0,0 +1,8 @@
+a=3
+b=2
+print("Suma numerelor a si b este:",a+b)
+print("Diferenta numerelor a si b este:",a-b)
+print("Catul impartirii nr a la nr b este:", a//b)
+print("Restul impartirii nr a la nr b este:",a/b)
+print("Produsul inmultirii numerelor a si b este:",a*b)
+print("Ridicarea la putere nr a-baza, nr b-exponentul:",a**b)
