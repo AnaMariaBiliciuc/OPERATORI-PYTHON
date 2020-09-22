aee6aade76afb09d0d466e736098d4917cf06c36
AnaMariaBiliciuc <70842937+AnaMariaBiliciuc@users.noreply.github.com>
2020-09-22 19:50:47 +0300

    Add files via upload

---
+++ problema 4.py
@@ -0,0 +1,7 @@
+n=4
+print("n metri transformati in centimetri sunt:", n*100)
+print("n kilograme transformate in miligrame sunt:", n*1000000)
+print("n ani transformati in luni sunt:", n*12)
+print("n ani transformati in saptamani sunt:",n*52)
+print("n ani transformati in zile sunt:" ,n*365)
+
