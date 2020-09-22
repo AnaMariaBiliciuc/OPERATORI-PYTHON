e0fdd62df4428b3654719acaac53214e4a70830e
AnaMariaBiliciuc <70842937+AnaMariaBiliciuc@users.noreply.github.com>
2020-09-22 18:05:12 +0300

    Add files via upload

---
+++ problema 3.py
@@ -0,0 +1,5 @@
+a=3
+print("Latura unui cub este:",a)
+print("Perimetrul unei suprafeti a cubului este:",a*4 )
+print("Aria unei totala a cubului este:", (a**2)*6)
+print("Volumul cubului este:",a**3)
