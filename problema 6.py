e0b587a0f082b393fb5b4f4f385ec59179357351
AnaMariaBiliciuc <70842937+AnaMariaBiliciuc@users.noreply.github.com>
2020-09-22 21:06:44 +0300

    Add files via upload

---
+++ problema 6.py
@@ -0,0 +1,4 @@
+n= 987
+print(" Catul impartirii numarului n la 9 este:", n//9)
+print(" Restul impartirii numarului n la 9 este:", n%9)
+print(" Ultima cifra a acestui numar este:", n mod 10 div 10)
