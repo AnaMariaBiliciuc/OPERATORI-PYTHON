39655e33060adb001c458b7f33fb3ac6881785e8
AnaMariaBiliciuc <70842937+AnaMariaBiliciuc@users.noreply.github.com>
2020-09-22 17:22:40 +0300

    Delete problema_2.py

--- problema_2.py
+++
@@ -1,7 +0,0 @@
-a=4
-b=3
-print("Suma, a+b=", a+ b)
-print("Diferenta numerelor este ,a-b=", a-b)
-print("Catul impartirii numerelor", a//b)
-print("Restul impartirii", a%b)
-print("Produsul impartirii numerelor este", a*b)
